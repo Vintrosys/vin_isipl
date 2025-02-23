b3f656bd39444abd3dde705598c9136fad97c715
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2025-02-23 17:38:48 +0530

    fix: PI - stock status and hsn columns

--- vin_isipl/install.py
+++ vin_isipl/install.py
@@ -5,13 +5,22 @@ def after_install():
 	create_custom_fields(get_custom_fields())
 	
 def get_custom_fields():
-    return {
+	return {
 		"Company": [
-            {
+			{
 				"fieldname": "quotation_left_logo",
 				"fieldtype": "Attach Image",
 				"label": "Quotation Left Logo",
 				"insert_after": "parent_company",
 			},
 		],
-    }
+		"Quotation Item": [
+			{
+				"fieldname": "stock_status",
+				"fieldtype": "Select",
+				"label": "Stock Status",
+				"options": "\nAvailable\nPartially Available\nNot Available",
+				"insert_after": "prevdoc_docname",
+			}
+		]
+	}
--- vin_isipl/uninstall.py
+++ vin_isipl/uninstall.py
@@ -4,10 +4,10 @@ import frappe
 from vin_isipl.install import get_custom_fields
 
 def before_uninstall():
-    print("Removing customizations created by the Vin ISIPL app...")
-    remove_custom_fields()
+	print("Removing customizations created by the Vin ISIPL app...")
+	remove_custom_fields()
 
-    click.secho("Vin ISIPL customizations have been removed successfully...", fg="green")
+	click.secho("Vin ISIPL customizations have been removed successfully...", fg="green")
 
 def remove_custom_fields():
 	for doctype, fields in get_custom_fields().items():
