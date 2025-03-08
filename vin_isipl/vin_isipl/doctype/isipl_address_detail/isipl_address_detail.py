3cefa7d38fcbdb82d4dd3bf4275144be557c9e24
kaviya.3111999@gmail.com <kaviya.3111999@gmail.com>
2025-03-08 15:38:18 +0530

    crm flow test & fixes

--- vin_isipl/vin_isipl/doctype/isipl_address_detail/isipl_address_detail.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Vintrosys and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class ISIPLAddressDetail(Document):
-	pass
