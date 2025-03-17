60a1efd2abffecd64a1b9638ede74a33a83a3661
kaviya.3111999@gmail.com <kaviya.3111999@gmail.com>
2025-03-17 10:01:18 +0530

    feat: org fetch, item template display

--- vin_isipl/utils/isipl_crm.py
+++ vin_isipl/utils/isipl_crm.py
@@ -4,20 +4,22 @@ from frappe.utils import  get_url_to_list, get_url_to_form
 
 @frappe.whitelist()
 def get_quotation_url(crm_deal, organization):
-    erpnext_crm_settings = frappe.get_single("ERPNext CRM Settings")
-    if not erpnext_crm_settings.enabled:
-        frappe.throw(_("ERPNext is not integrated with the CRM"))
-    customer = frappe.db.get_value("Customer", {"crm_deal": crm_deal})
-    if not customer:
-            org = frappe.db.get_value("CRM Deal", crm_deal,'organization')
-            deals = frappe.get_list("CRM Deal", {'organization': org})
-            for deal in deals:
-                customer = frappe.db.exists("Customer", {"crm_deal": deal['name']})
-                if customer:
-                    break
-    if not erpnext_crm_settings.is_erpnext_in_different_site:
-        quotation_url = get_url_to_list("Quotation")
-        return f"{quotation_url}/new?quotation_to=Customer&party_name={customer}&company={erpnext_crm_settings.erpnext_company}"
+	erpnext_crm_settings = frappe.get_single("ERPNext CRM Settings")
+	if not erpnext_crm_settings.enabled:
+		frappe.throw(_("ERPNext is not integrated with the CRM"))
+	customer = frappe.db.get_value("Customer", {"crm_deal": crm_deal})
+	if not customer:
+			org = frappe.db.get_value("CRM Deal", crm_deal,'organization')
+			deals = frappe.get_list("CRM Deal", {'organization': org})
+			for deal in deals:
+				customer = frappe.db.exists("Customer", {"crm_deal": deal['name']})
+				if customer:
+					break
+	if not customer:
+			customer = org
+	if not erpnext_crm_settings.is_erpnext_in_different_site:
+		quotation_url = get_url_to_list("Quotation")
+		return f"{quotation_url}/new?quotation_to=Customer&party_name={customer}&company={erpnext_crm_settings.erpnext_company}"
     
 @frappe.whitelist()
 def get_customer_link(crm_deal):
@@ -34,4 +36,6 @@ def get_customer_link(crm_deal):
 				customer = frappe.db.exists("Customer", {"crm_deal": deal['name']})
 				if customer:
 					break
+		if not customer:
+			customer = org
 		return get_url_to_form("Customer", customer) if customer else ""
