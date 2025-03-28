43d49cc5d74133ff30afce716a17561d452424d3
kaviya.3111999@gmail.com <kaviya.3111999@gmail.com>
2025-03-28 12:47:17 +0530

    fix: version tracker ux

--- vin_isipl/public/js/proforma_invoice.js
+++ vin_isipl/public/js/proforma_invoice.js
@@ -1,12 +1,13 @@
 frappe.ui.form.on('Quotation', {
     refresh: function (frm) {
         if (!frm.is_new()) {           
-            frm.add_custom_button('Save to Table', function () {
+            frm.add_custom_button('Save PDF', function () {
                 frappe.call({
                     method: "vin_isipl.utils.pi_version_tracker.save_to_table",
                     args: {
                         quotation_name: frm.doc.name
                     },
+                    freeze: true,
                     callback: function () {
                         frappe.show_alert({ message: __("Version Tracker Updated!"), indicator: 'green' });
 
