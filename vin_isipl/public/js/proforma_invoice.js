2ec4d2e6f924f444cb30262fb529bbee7395a027
SaranyaSwaminathan <saranyaswaminathan89@gmail.com>
2025-04-01 10:24:58 +0530

    Removed reduntant characters in script
    Resolved Merger Conflicts

--- vin_isipl/public/js/proforma_invoice.js
+++ vin_isipl/public/js/proforma_invoice.js
@@ -17,3 +17,19 @@ frappe.ui.form.on('Quotation', {
         }
     }
 });
+
+function fetch_sales_person(frm, deal_owner) {    
+    frappe.db.get_value('Employee', { user_id: deal_owner }, 'name')
+        .then(emp => {
+            if (emp.message && emp.message.name) {               
+                frappe.db.get_value('Sales Person', { employee: emp.message.name }, 'name')
+                    .then(sp => {
+                        frm.set_value('sales_person', sp.message.name);
+                    });
+            } else {
+                frm.set_value('sales_person', '');
+            }
+            frm.refresh_field('sales_person');
+        });
+}
+
