818c145770ac184b6b2c1076571ec246b8e38e57
sathishkumarvintro <sathishsubramaniyam18@gmail.com>
2025-08-18 14:57:23 +0530

    redploy fix

--- vin_isipl/__init__.py
+++ vin_isipl/__init__.py
@@ -1,4 +1,4 @@
 __version__ = "0.0.1"
-from vin_isipl.patches.custom_make_contact_patch import apply_monkey_patch
+# from vin_isipl.patches.custom_make_contact_patch import apply_monkey_patch
 
-apply_monkey_patch()
+# apply_monkey_patch()
