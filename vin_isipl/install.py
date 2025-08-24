5ffedb807777f90311dea8f4e51fc6ecc1d5d953
sathishkumarvintro <sathishsubramaniyam18@gmail.com>
2025-08-24 10:52:37 +0530

    fix: fixtures issue, custom field creation

--- vin_isipl/install.py
+++ vin_isipl/install.py
@@ -2,7 +2,8 @@ from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
 
 def after_install():
-	create_custom_fields(get_custom_fields())
+	pass
+	# create_custom_fields(get_custom_fields())
 	
 def get_custom_fields():
 	return {
