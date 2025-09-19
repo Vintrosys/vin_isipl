5feb8333c2f4ae6b0f4a5067b0492fe9e6486d59
frappe <saranyaswaminathan89@gmail.com>
2025-09-19 17:40:44 +0530

    Whatsapp Integration

---
+++ vin_isipl/vin_isipl/doctype/whynoo_settings/whynoo_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Vintrosys and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Whynoo Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
