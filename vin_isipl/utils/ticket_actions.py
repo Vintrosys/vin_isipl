7f423c4cf01b282dc0c55b5289907e9958cfb784
frappe <saranyaswaminathan89@gmail.com>
2025-09-19 20:55:18 +0530

    Check In, Check Out, Time Spent

---
+++ vin_isipl/utils/ticket_actions.py
@@ -0,0 +1,46 @@
+import frappe
+from frappe.utils import time_diff_in_hours, now_datetime
+from frappe import _
+
+@frappe.whitelist()
+def checkout_and_compute(ticket):
+
+    try:
+        doc = frappe.get_doc("HD Ticket", ticket)
+
+        now = now_datetime()
+        doc.custom_check_out = now
+
+        if doc.custom_check_in:            
+            hours = time_diff_in_hours(doc.custom_check_out, doc.custom_check_in)
+            doc.custom_time_spent = round(hours, 2)
+        doc.save(ignore_permissions=True)
+        frappe.db.commit()
+        return {
+            "check_out": doc.custom_check_out,
+            "time_spent": doc.custom_time_spent
+        }
+    
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(), "CheckOut Error")
+        frappe.throw("Failed to check out: " + str(e))
+
+
+@frappe.whitelist()
+def set_check_in(ticket):
+    try :
+        doc = frappe.get_doc("HD Ticket", ticket)
+        now = now_datetime()   
+        doc.custom_check_in = now
+        doc.save(ignore_permissions=True)
+        frappe.db.commit()
+        return {
+            "check_in": str(now)
+        }
+    
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(), "CheckIn Error")
+        frappe.throw("Failed to check in: " + str(e))
+    
+
+
