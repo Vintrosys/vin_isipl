3b50c79dcc84514335ba3af43350e321b931443e
sathishkumarvintro <sathishsubramaniyam18@gmail.com>
2026-01-14 20:19:27 +0530

    fix: prod exports

---
+++ vin_isipl/vin_isipl/doctype/hd_ticket_spare_status/hd_ticket_spare_status.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Vintrosys and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("HD Ticket Spare Status", {
+// 	refresh(frm) {
+
+// 	},
+// });
