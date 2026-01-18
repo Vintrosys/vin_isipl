c844c2ede8718691feccb09d476f14092eea16a5
KaviyaPeriyasamy <kaviyaperiyasamy22@gmail.com>
2026-01-18 18:11:17 +0530

    feat:market segment

---
+++ vin_isipl/vin_isipl/doctype/isipl_crm_market_segment/isipl_crm_market_segment.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Vintrosys and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("ISIPL CRM Market Segment", {
+// 	refresh(frm) {
+
+// 	},
+// });
